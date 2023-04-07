498b13a381ddf19ea4ad3b29dea2ae116848e7d8
lamnguyen <lamnguyen9964@gmail.com>
2023-04-07 22:45:59 +0700

    create config webpack reactjs

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>Tôi tên là Nguễn Thị Kim Lam </p>
+      <p>Sinh năm 1999</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
